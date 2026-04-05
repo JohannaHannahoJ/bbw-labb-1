512d4661f6fe79a17aa2db6c897faa65844097a2
JohannaHannahoJ <joha2102@student.miun.se>
2026-04-05 15:58:05 +0200

    Add body parser to handle form POST data

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser"); // Möjlighet att läsa in formulärdata
 const app = express();
 const port = 3100;
 
 // statiska filer ska hamna i public sökväg: "/"
 app.use(express.static("public"));
+// använder body parser som middleware skicka med ett objekt
+app.use(bodyParser.urlencoded({extended: true}));
 // väljer ejs för att läsa tempaltes
 app.set("view engine", "ejs");
 
@@ -14,6 +17,10 @@ app.get("/", (req, res) => {
 });
 
 app.get("/add", (req, res) => {
+    res.render("add");
+});
+
+app.post("/add", (req, res) => {
     res.render("add");
 });
 
